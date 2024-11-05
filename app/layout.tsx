867254719626a7bc4add37458c42cf086cc71a6d
Cavin <cavinlarry001@gmail.com>
2024-11-05 23:05:05 +0300

    Fix the data to load locally other than previous firebase RD

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,30 @@
 'use client';
-import './globals.css'
-import { Quicksand } from '@next/font/google'
-import { ThemeProvider } from 'next-themes'
+import './globals.css';
+import { Quicksand } from '@next/font/google';
+import ClientThemeProvider from '@/components/ClientThemeProvider';
 import { Analytics } from '@vercel/analytics/react';
 
 const quicksand = Quicksand({
   subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700']
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head />
-      <ThemeProvider attribute='class' defaultTheme='light'>
-        <body className={`${quicksand.className} antialiased bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden`}>
-          {/* <body className='bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden'> */}
+      <body
+        className={`${quicksand.className} antialiased bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden`}
+      >
+        <ClientThemeProvider>
           {children}
-          <Analytics />
-        </body>
-      </ThemeProvider>
+        </ClientThemeProvider>
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
