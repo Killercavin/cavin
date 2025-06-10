4668cf9d1379e6de363d64c758ba4bd7ce447ba4
Cavin <cavinlarry001@gmail.com>
2025-06-10 14:18:34 +0300

    delete: cleaned the repo for a deep fix

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['res.cloudinary.com', 'firebasestorage.googleapis.com', 'img.icons8.com', 'raw.githubusercontent.com', 'i.imgur.com', 'img.freepik.com','media.geeksforgeeks.org']
-  }
-}
-
-module.exports = nextConfig
